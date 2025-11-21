ba4c6d295421db28bbcb26cf776f6e96c84aaf0a
Mary rani <mary.rani@stackroute.in>
2025-11-21 11:25:45 +0530

    changed service code

--- src/main/java/com/stackroute/productjpa/service/ProductServiceImpl.java
+++ src/main/java/com/stackroute/productjpa/service/ProductServiceImpl.java
@@ -27,7 +27,7 @@ public class ProductServiceImpl implements ProductService {
 	@Override
 	public List<Product> viewAllProducts() {
 	List<Product>  products=  productrepo.findAll();
-		return products;
+		return "good";
 	}
 
 	@Override
